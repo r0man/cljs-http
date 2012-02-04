c0bf16dede194feacf39025826f256f4ebc88ea4
Roman <roman@burningswell.com>
2012-02-04 23:43:06 +0100

    Configured cljsbuild.

--- project.clj
+++ project.clj
@@ -1,11 +1,8 @@
-(defproject cljs-http "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject cljs-http "0.0.1-SNAPSHOT"
+  :description "A ClojureScript HTTP library."
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :dev-dependencies [[lein-cljsbuild "0.0.11"]]
   :hooks [leiningen.cljsbuild]
-  :cljsbuild [{:source-path "src/cljs"
-               :jar true
+  :cljsbuild [{:source-path "src"
                :compiler {:output-to "cljs-http.js"
-                          ;; :optimizations :advanced
-                          :optimizations :whitespace
-                          :pretty-print true}}])
+                          :optimizations :advanced}}])
