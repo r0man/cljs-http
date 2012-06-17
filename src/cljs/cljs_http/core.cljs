2d8633f389104b39adaa1577a71e53cc7756bf15
Roman <roman@burningswell.com>
2012-06-17 12:38:55 +0200

    Removed prn.

--- src/cljs/cljs_http/core.cljs
+++ src/cljs/cljs_http/core.cljs
@@ -25,10 +25,6 @@
    bodies."
   [{:keys [request-method scheme server-name server-port uri query-string
            headers content-type character-encoding body on-complete] :as request}]
-  (prn request-method)
-  (prn server-name)
-  (prn server-port)
-  (prn uri)
   (let [xhr (goog.net.XhrIo.)]
     (.setWithCredentials xhr true)
     (if on-complete
