688d7b092fdc87c5a865de1fe3b13d9ee0f3b851
Roman <roman@burningswell.com>
2012-06-17 12:38:46 +0200

    Fix firefox repl.

--- project.clj
+++ project.clj
@@ -10,13 +10,13 @@
                                    :pretty-print false}
                         :source-path "src/cljs"}
                        {:compiler {:output-to "target/cljs-http-test.js"
-                                   ;; :optimizations :advanced
+                                   :optimizations :advanced
                                    :pretty-print true}
                         :jar true
                         :source-path "test/cljs"}]
               :repl-listen-port 9000
               :repl-launch-commands
               {"chromium" ["chromium" "http://localhost:9000/"]
-               "firefox" ["firefox" "http://http://localhost:9000/"]}
+               "firefox" ["firefox" "http://localhost:9000/"]}
               :test-commands {"unit" ["./test-cljs.sh"]}}
   :source-path "src/clj")
