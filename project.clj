5eb258a4b7662c07e1ab84e4daddfb9fc56477ab
Roman Scherer <roman.scherer@burningswell.com>
2013-04-20 17:18:03 +0200

    Release 0.0.4

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject cljs-http "0.0.4-SNAPSHOT"
+(defproject cljs-http "0.0.4"
   :description "A ClojureScript HTTP library."
   :url "http://github.com/r0man/cljs-http"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :profiles {:dev {:dependencies [[com.cemerick/clojurescript.test "0.0.3"]
                                   [com.cemerick/piggieback "0.0.4"]]}}
   :plugins [[lein-cljsbuild "0.3.0"]]
