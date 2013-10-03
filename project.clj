d03a53f00fc516f7a77382695600a20030983d8e
Roman Scherer <roman.scherer@burningswell.com>
2013-10-03 17:34:51 +0200

    lein-release plugin: bumped version from 0.1.0 to 0.1.1-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-http "0.1.0"
+(defproject cljs-http "0.1.1-SNAPSHOT"
   :description "A ClojureScript HTTP library."
   :url "http://github.com/r0man/cljs-http"
   :license {:name "Eclipse Public License"
