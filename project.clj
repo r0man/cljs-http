b1f378292be16fcff41f7618aad69c891e8d1dc7
Roman Scherer <roman.scherer@burningswell.com>
2013-10-13 12:04:55 +0200

    Updated dependencies.

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "http://github.com/r0man/cljs-http"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[noencore "0.1.5"]
+  :dependencies [[noencore "0.1.6"]
                  [org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1909"]
+                 [org.clojure/clojurescript "0.0-1934"]
                  [org.clojure/core.async "0.1.0-SNAPSHOT"]]
-  :profiles {:dev {:dependencies [[com.cemerick/clojurescript.test "0.0.4"]]
-                   :plugins [[com.cemerick/austin "0.1.1"]]}}
+  :profiles {:dev {:plugins [[com.cemerick/austin "0.1.1"]
+                             [com.cemerick/clojurescript.test "0.1.0"]]}}
   :plugins [[lein-cljsbuild "0.3.2"]]
   :hooks [leiningen.cljsbuild]
   :min-lein-version "2.0.0"
@@ -27,5 +27,5 @@
               :repl-launch-commands
               {"chromium" ["chromium" "http://localhost:9000/"]
                "firefox" ["firefox" "http://localhost:9000/"]}
-              :test-commands {"unit-tests" ["runners/phantomjs.js" "target/cljs-http-test.js"]}}
+              :test-commands {"unit-tests" ["phantomjs" :runner "target/cljs-http-test.js"]}}
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"})
