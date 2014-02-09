9089da91a2a1917b008e6cdab60982e0179bd7bd
Roman Scherer <roman.scherer@burningswell.com>
2014-02-09 13:24:53 +0100

    lein-release plugin: preparing 0.1.7 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-http "0.1.7-SNAPSHOT"
+(defproject cljs-http "0.1.7"
   :description "A ClojureScript HTTP library."
   :url "http://github.com/r0man/cljs-http"
   :license {:name "Eclipse Public License"
