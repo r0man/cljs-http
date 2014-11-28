2177fc7c6f6b8923a34ae54398fda05196d758b7
Roman Scherer <roman@burningswell.com>
2014-11-28 17:35:26 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "http://github.com/r0man/cljs-http"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[noencore "0.1.16"]
+  :dependencies [[noencore "0.1.18"]
                  [org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2371"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [com.cognitect/transit-cljs "0.8.188"]]
+                 [com.cognitect/transit-cljs "0.8.192"]]
   :profiles {:dev {:plugins [[com.cemerick/austin "0.1.3"]]}}
   :plugins [[com.cemerick/clojurescript.test "0.3.0"]
             [lein-cljsbuild "1.0.3"]]
