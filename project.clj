d869a553b60147f8b95fb445226f80b8df182715
Roman Scherer <roman@burningswell.com>
2015-03-28 13:05:02 +0100

    Version 0.1.29

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-http "0.1.29-SNAPSHOT"
+(defproject cljs-http "0.1.29"
   :description "A ClojureScript HTTP library."
   :url "https://github.com/r0man/cljs-http"
   :license {:name "Eclipse Public License"
