9abb7308afea35915b0b6716d7bf0919c228ec7d
Roman Scherer <roman@burningswell.com>
2015-03-28 13:00:55 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -1,22 +1,21 @@
 (defproject cljs-http "0.1.29-SNAPSHOT"
   :description "A ClojureScript HTTP library."
-  :url "http://github.com/r0man/cljs-http"
+  :url "https://github.com/r0man/cljs-http"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[noencore "0.1.18"]
+            :url "https://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[noencore "0.1.19"]
                  [org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2850"]
+                 [org.clojure/clojurescript "0.0-3165"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [com.cognitect/transit-cljs "0.8.205"]]
   :plugins [[com.cemerick/clojurescript.test "0.3.0"]
             [lein-cljsbuild "1.0.5"]]
   :aliases {"test" ["do" "clean," "cljsbuild" "test"]
-            "test-ancient" ["test"]}
+            "test-ancient" ["do" "clean," "cljsbuild" "test"]}
   :min-lein-version "2.0.0"
   :deploy-repositories [["releases" :clojars]]
   :cljsbuild {:builds [{:compiler {:output-to "target/testable.js"
                                    :optimizations :advanced
                                    :pretty-print true}
                         :source-paths ["test"]}]
-              :test-commands {"phantom" ["phantomjs" :runner "target/testable.js"]}}
-  :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"})
+              :test-commands {"phantom" ["phantomjs" :runner "target/testable.js"]}})
