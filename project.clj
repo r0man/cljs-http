23c79837261b702e9a20efbd1eb730a15ad32105
Roman Scherer <roman@burningswell.com>
2015-06-03 14:29:15 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "https://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[noencore "0.1.19"]
-                 [org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojure "1.7.0-RC1"]
                  [org.clojure/clojurescript "0.0-3165" :scope "provided"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [com.cognitect/transit-cljs "0.8.205"]]
   :plugins [[com.cemerick/clojurescript.test "0.3.3"]
-            [lein-cljsbuild "1.0.5"]]
+            [lein-cljsbuild "1.0.6"]]
   :aliases {"test" ["do" "clean," "cljsbuild" "test"]
             "test-ancient" ["do" "clean," "cljsbuild" "test"]}
   :min-lein-version "2.0.0"
