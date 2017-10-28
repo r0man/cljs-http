d2efb79295151cb72a3d9a57209534a2621bd06e
r0man <roman@burningswell.com>
2017-10-28 11:56:23 +0200

    Version 0.1.44

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-http "0.1.44-SNAPSHOT"
+(defproject cljs-http "0.1.44"
   :description "A ClojureScript HTTP library."
   :url "https://github.com/r0man/cljs-http"
   :license {:name "Eclipse Public License"
