3bde7ab4f6ee320486dd6801d6d034b6ad360ba2
r0man <roman@burningswell.com>
2018-04-02 10:23:11 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,14 +3,14 @@
   :url "https://github.com/r0man/cljs-http"
   :license {:name "Eclipse Public License"
             :url "https://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[noencore "0.3.3"]
-                 [org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.229" :scope "provided"]
-                 [org.clojure/core.async "0.3.442"]
+  :dependencies [[noencore "0.3.4"]
+                 [org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.10.238" :scope "provided"]
+                 [org.clojure/core.async "0.4.474"]
                  [org.clojure/tools.namespace "0.2.11"]
-                 [com.cognitect/transit-cljs "0.8.239"]]
-  :plugins [[lein-cljsbuild "1.1.4"]
-            [lein-doo "0.1.9"]]
+                 [com.cognitect/transit-cljs "0.8.256"]]
+  :plugins [[lein-cljsbuild "1.1.7"]
+            [lein-doo "0.1.10"]]
   :doo {:paths {:karma "node_modules/.bin/karma"}}
   :aliases {"test" ["do"
                     ["clean"]
@@ -36,4 +36,4 @@
                 :source-paths ["src" "test"]}]}
   :deploy-repositories [["releases" :clojars]]
   :min-lein-version "2.0.0"
-  :profiles {:dev {:dependencies [[doo "0.1.9"]]}})
+  :profiles {:dev {:dependencies [[doo "0.1.10"]]}})
