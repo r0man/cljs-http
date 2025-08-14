3a622d8cd2df5151150280a53a15a0bcf1ba4ae7
Roman Scherer <roman@burningswell.com>
2025-08-14 13:27:27 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "https://github.com/r0man/cljs-http"
   :license {:name "Eclipse Public License"
             :url "https://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[noencore "0.3.8"]
-                 [org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.238" :scope "provided"]
-                 [org.clojure/core.async "0.4.474"]
-                 [org.clojure/tools.namespace "0.2.11"]
-                 [com.cognitect/transit-cljs "0.8.256"]]
+  :dependencies [[noencore "0.4.0"]
+                 [org.clojure/clojure "1.12.1"]
+                 [org.clojure/clojurescript "1.12.42" :scope "provided"]
+                 [org.clojure/core.async "1.8.741"]
+                 [org.clojure/tools.namespace "1.5.0"]
+                 [com.cognitect/transit-cljs "0.8.280"]]
   :plugins [[lein-cljsbuild "1.1.7"]
             [lein-doo "0.1.10"]]
   :doo {:paths {:karma "node_modules/.bin/karma"}}
@@ -36,4 +36,4 @@
                 :source-paths ["src" "test"]}]}
   :deploy-repositories [["releases" :clojars]]
   :min-lein-version "2.0.0"
-  :profiles {:dev {:dependencies [[doo "0.1.10"]]}})
+  :profiles {:dev {:dependencies [[doo "0.1.11"]]}})
