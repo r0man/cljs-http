9bfc79c01768a5c7a6c6c5d923fc6f27b02d54b1
Roman Scherer <roman@burningswell.com>
2025-08-14 13:56:23 +0200

    Version 0.1.50-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-http "0.1.49"
+(defproject cljs-http "0.1.50-SNAPSHOT"
   :description "A ClojureScript HTTP library."
   :url "https://github.com/r0man/cljs-http"
   :license {:name "Eclipse Public License"
